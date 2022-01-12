bf8b2dbb9ebcec925b158af06e328bd0d7ee4398
DeRose Brutus <derose_brutus@hotmail.com>
2022-01-12 14:31:40 -0500

    Create movement controller

---
+++ DreamsVR/Assets/MovementController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MovementController : MonoBehaviour
+{
+    public VRHands hands;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
