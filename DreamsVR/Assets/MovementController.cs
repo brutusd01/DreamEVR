0fb80af2baaeb7704e1c459161ed47a2014d3804
DeRose Brutus <derose_brutus@hotmail.com>
2022-01-14 17:24:00 -0500

    Update MovementController script to hide teleport rays

--- DreamsVR/Assets/MovementController.cs
+++ DreamsVR/Assets/MovementController.cs
@@ -5,16 +5,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovementController : MonoBehaviour
 {
-    public VRHands hands;
+    public XRController leftTele;
+    public XRController rightTele;
+    public InputHelpers.Button teleportInput;
+    public float deadZone = .1f;
 
-    void Start()
-    {
-        
-    }
-
-    
     void Update()
     {
-        
+        if(leftTele)
+        {
+            leftTele.gameObject.SetActive(CheckActive(leftTele));
+        }
+        if (rightTele)
+        {
+            rightTele.gameObject.SetActive(CheckActive(rightTele));
+        }
+    }
+
+    public bool CheckActive(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportInput, out bool isPressed, deadZone);
+        return isPressed;
     }
 }
