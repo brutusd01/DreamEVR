fdb2a099da6f43a11850dc426fff0c9949c2e1e6
DeRose Brutus <derose_brutus@hotmail.com>
2022-01-14 23:58:48 -0500

    Add continuous movement to movement controller

--- DreamsVR/Assets/MovementController.cs
+++ DreamsVR/Assets/MovementController.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovementController : MonoBehaviour
 {
+    //TODO: Be able to set MoveType in game.
+    public enum MoveType {Teleport, Continuous }
+    private MoveType move;
+    [Header("Teleport Movement")]
+    public bool Teleport = true;
     public XRController leftTele;
     public XRController rightTele;
     public InputHelpers.Button teleportInput;
     public float deadZone = .1f;
+    [Header("Continuous")]
+    public float speed = 1.5f;
+    public float vSpeed;
+    public XRNode input;
+    private Vector2 inputAxis;
+    private CharacterController character;
 
+    private void Start()
+    {
+        if(!character)character = GetComponent<CharacterController>();
+    }
     void Update()
     {
-        if(leftTele)
+        //if (move==MoveType.Teleport)
+        if(Teleport)
         {
-            leftTele.gameObject.SetActive(CheckActive(leftTele));
+            if (leftTele)
+            {
+                leftTele.gameObject.SetActive(CheckActive(leftTele));
+            }
+            if (rightTele)
+            {
+                rightTele.gameObject.SetActive(CheckActive(rightTele));
+            }
         }
-        if (rightTele)
+        else
         {
-            rightTele.gameObject.SetActive(CheckActive(rightTele));
+            InputDevice device = InputDevices.GetDeviceAtXRNode(input);
+            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!Teleport)
+        {
+            Vector3 moveVector = new Vector3(inputAxis.x, vSpeed, inputAxis.y);
+            character.Move(moveVector * Time.fixedDeltaTime * speed);
         }
     }
 
