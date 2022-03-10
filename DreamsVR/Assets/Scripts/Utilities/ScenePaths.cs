7b24d171bfab04aa1e308da8d0f94db780667065
DeRose Brutus <derose_brutus@hotmail.com>
2022-03-10 17:28:39 -0500

    Add scenes to ScenePaths

--- DreamsVR/Assets/Scripts/Utilities/ScenePaths.cs
+++ DreamsVR/Assets/Scripts/Utilities/ScenePaths.cs
@@ -4,5 +4,15 @@ namespace Utilities
     {
         public const string BasePath = "Assets/Scenes";
         public const string Extension = ".unity";
+        
+        
+        public const string
+            BootstrapName = "Bootstrap",
+            BootstrapPath = BasePath + BootstrapName + Extension;
+        
+        
+        public const string
+            TestSceneName = "TestScene",
+            TestScenePath = BasePath + TestSceneName + Extension;
     }
 }
