656637161b4545707526fc646bb09f9f6d96f6d4
DeRose Brutus <derose_brutus@hotmail.com>
2022-03-10 18:07:07 -0500

    Create Editor Script to boot up scenes

--- DreamsVR/Assets/Scripts/Utilities/QuickActions.cs
+++ DreamsVR/Assets/Scripts/Utilities/QuickActions.cs
@@ -1,13 +1,35 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEngine;
 using Utilities;
 
 
-public class QuickActions : MonoBehaviour
+public class QuickActions
 {
 
-    static void BootScenes()
+    private const string MenuName = "QuickActions";
+
+    [MenuItem(MenuName + "/Scenes/Open Bootstrap")]
+    private static void BootScenes()
     {
-        
+        CloseAndOpenScenes(new[]
+        {
+            ScenePaths.BootstrapPath,
+            ScenePaths.TestScenePath
+        });
+    }
+
+    static void CloseAndOpenScenes(IEnumerable<string> scenePaths)
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            var i = 0;
+            foreach (string scene in scenePaths)
+            {
+                if (i == 0) EditorSceneManager.OpenScene(scene, OpenSceneMode.Single);
+                else EditorSceneManager.OpenScene(scene, OpenSceneMode.Additive);
+                i++;
+            }
+        }
     }
 }
