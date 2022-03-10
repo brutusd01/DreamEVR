8d08ed710679813f924365dd2e7c1057fa6f355b
DeRose Brutus <derose_brutus@hotmail.com>
2022-03-10 18:06:52 -0500

    Fix BasePath string in ScenePaths.cs

--- DreamsVR/Assets/Scripts/Utilities/ScenePaths.cs
+++ DreamsVR/Assets/Scripts/Utilities/ScenePaths.cs
@@ -2,7 +2,7 @@ namespace Utilities
 {
     public class ScenePaths
     {
-        public const string BasePath = "Assets/Scenes";
+        public const string BasePath = "Assets/Scenes/";
         public const string Extension = ".unity";
         
         
